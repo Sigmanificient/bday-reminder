2c336fb2e2dbde6ff6ff437b81e301bccc922020
Sigmanificient <edhyjox@gmail.com>
2021-06-06 16:03:05 +0200

    working db connection

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
+import sqlite3
 from flask import Flask
+from flask import g
 
+DATABASE = r'db/db.sqlite'
 app = Flask(__name__)
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(_):
+    db = getattr(g, '_database', None)
+
+    if db is not None:
+        db.close()
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    cur = get_db().cursor()
+
+    result = cur.execute("select * from sqlite_master").fetchall()
+    cur.close()
+    return repr(result)
 
 
 if __name__ == '__main__':
