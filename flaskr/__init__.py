f1adbe5da96b88360a8f211521048a67f6267d12
Corentin384 <cocotex38@gmail.com>
2021-06-14 18:26:38 +0200

    link index

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app(test_config=None):
@@ -25,9 +25,9 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
+    @app.route('/')
     def hello():
-        return 'Hello, World!'
+        return render_template('index.html')
 
     from . import db
     db.init_app(app)
