aaa1845ac84ac98bd179ef68446f47a57ec3dac7
Yohann Boniface <edhyjox@gmail.com>
2021-08-08 23:42:11 +0200

    shorten & optimized search form

--- src/static/js/search_birthday.js
+++ src/static/js/search_birthday.js
@@ -1,15 +1,15 @@
-window.onload = () => {
-    const searchBirthdayForm = document.getElementById('birthday_form')
-    searchBirthdayForm.addEventListener(
-        'submit', async (e) => {
-            e.preventDefault()
+document.getElementById('birthday_form').addEventListener(
+    'submit', async (e) => {
+        e.preventDefault()
 
-            let searchUser = document.getElementById('search').value
+        let searchUser = e.target.parentNode.querySelector('#search').value
+
+        if (searchUser !== '') {
             let url = `${window.location}api/search/${searchUser}`
 
             await fetch(url)
                 .then((r) => r.json())
                 .then((json) => console.log(json))
         }
-    )
-}
+    }
+)
