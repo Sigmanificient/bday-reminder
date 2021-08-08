808402e537175ea4aba008753c7c7bd4d7bf1fd3
Yohann Boniface <edhyjox@gmail.com>
2021-08-08 23:53:50 +0200

    removed unused js code

--- src/static/js/delete_birthday.js
+++ src/static/js/delete_birthday.js
@@ -1,20 +1,3 @@
-/*
-const forms = document.querySelectorAll('.delete-form')
-
-forms.forEach(
-    (form, index) => {
-        form.addEventListener(
-            'submit', async (e) => {
-                e.preventDefault()
-                form.parentElement.remove()
-
-                await fetch(`/delete/${index}`)
-            }
-        )
-    }
-)
- */
-
 document.querySelectorAll('button[data-id]').forEach(
     (btn) => {
         btn.addEventListener(
