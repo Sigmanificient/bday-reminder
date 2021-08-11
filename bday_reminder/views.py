c2b7de44d00f03cfb3bc3bbeba6710b9eb693a8e
Yohann Boniface <edhyjox@gmail.com>
2021-08-11 19:08:15 +0200

    Adding base routes

--- bday_reminder/views.py
+++ bday_reminder/views.py
@@ -1,3 +1,28 @@
 from flask import Blueprint, render_template
 
+from . import User
+
 views = Blueprint("views", __name__)
+
+
+@views.route('/', methods=('GET', 'POST'))
+def index_page():
+    return render_template('index.jinja2')
+
+
+@views.route('/legal')
+def legal_page():
+    return render_template('legal.jinja2')
+
+
+@views.route('/api/search/<user>')
+def search_user(user):
+    found_user = User.query.filter_by(pseudo=user).first()
+    if not found_user:
+        return {}
+
+    return {
+        'id': found_user.id,
+        'name': found_user.pseudo,
+        'birthday': found_user.birthday
+    }
