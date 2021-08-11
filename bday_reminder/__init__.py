a4cacaac5ed74e57328a8ce774ca7b4afcace918
Yohann Boniface <edhyjox@gmail.com>
2021-08-11 18:37:40 +0200

    Adding db

--- bday_reminder/__init__.py
+++ bday_reminder/__init__.py
@@ -1,8 +1,22 @@
+from os import path
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+DB_PATH = 'bday.db'
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQL_ALCHEMY_DATABASE_URL'] = f'sqlite:///{DB_PATH}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    if not path.exists(DB_PATH):
+        db.create_all()
 
     from .views import views
     app.register_blueprint(views, url_prefix='/')
