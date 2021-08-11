b0e144652d6d3632d9815a4d5f38e66805a49bae
Yohann Boniface <edhyjox@gmail.com>
2021-08-11 19:44:06 +0200

    Reintroducing db path check & typing

--- bday_reminder/__init__.py
+++ bday_reminder/__init__.py
@@ -8,16 +8,18 @@ DB_PATH = 'bday.db'
 db = SQLAlchemy()
 
 
-def create_app():
+def create_app() -> Flask:
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_PATH}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Importing db models for the db to initialize.
-    from bday_reminder.models import Birthday, User
-
     db.init_app(app=app)
-    db.create_all(app=app)
+
+    if not path.isfile(DB_PATH):
+        # Importing db models for the db to initialize.
+        from bday_reminder.models import Birthday, User
+
+        db.create_all(app=app)
 
     from .views import views
     app.register_blueprint(views, url_prefix='/')
