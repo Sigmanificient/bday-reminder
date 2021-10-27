93053ff6f53004e139225d4356554121e2f4bffb
Corentin384 <texiercorentin@orange.fr>
2021-10-27 17:07:17 +0200

    Go back to old hash method (because problems of commits)

--- bday_reminder/security.py
+++ bday_reminder/security.py
@@ -3,4 +3,4 @@ import hashlib
 
 def sha512(password: str) -> str:
     """Return a sha512 hash of the given password."""
-    return hashlib.blake2b(password.encode('utf-8')).hexdigest()
+    return hashlib.sha512(password.encode('utf-8')).hexdigest()
