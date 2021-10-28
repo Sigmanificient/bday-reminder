9c871323462598e35ce3637c210b455792d2c99f
sigmanificient <sigmanificient@gmail.com>
2021-10-28 23:15:34 +0200

    :fire: Removing event `e`

--- bday_reminder/static/js/delete_birthday.js
+++ bday_reminder/static/js/delete_birthday.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('button[data-id]').forEach(
     (btn) => {
         btn.addEventListener(
-            'click', async (e) => {
+            'click', async () => {
                 btn.parentElement.remove()
                 await fetch(`/delete/${btn.getAttribute('data-id')}`)
             }
