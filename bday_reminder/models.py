d8a51f05b1d0e00edf6339b51c1c0dd12a6a723e
sigmanificient <edhyjox@gmail.com>
2022-02-24 23:35:28 +0100

    :art: Reformat code

--- bday_reminder/models.py
+++ bday_reminder/models.py
@@ -2,16 +2,16 @@ from bday_reminder import db
 
 
 class Birthday(db.Model):
-    """A Table to store user's friends birthdays."""
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer)
-    person_name = db.Column(db.String(32))
-    person_birthday = db.Column(db.String(10))
+	"""A Table to store user's friends birthdays."""
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	user_id = db.Column(db.Integer)
+	person_name = db.Column(db.String(32))
+	person_birthday = db.Column(db.String(10))
 
 
 class User(db.Model):
-    """A Table to store user's app accounts."""
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    pseudo = db.Column(db.String(32))
-    password = db.Column(db.String(128))
-    birthday = db.Column(db.String(10), default=None)
+	"""A Table to store user's app accounts."""
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	pseudo = db.Column(db.String(32))
+	password = db.Column(db.String(128))
+	birthday = db.Column(db.String(10), default=None)
--- bday_reminder/security.py
+++ bday_reminder/security.py
@@ -2,5 +2,5 @@ import hashlib
 
 
 def blake2b(password: str) -> str:
-    """Return a sha512 hash of the given password."""
-    return hashlib.blake2b(password.encode('utf-8')).hexdigest()
+	"""Return a sha512 hash of the given password."""
+	return hashlib.blake2b(password.encode('utf-8')).hexdigest()
